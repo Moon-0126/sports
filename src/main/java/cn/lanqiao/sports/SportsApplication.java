83e35389e0615107542d4bcd0a23e6e137b620f7
YUE <2278674334@qq.com>
2024-12-23 15:23:18 +0800

    1

---
+++ src/main/java/cn/lanqiao/sports/SportsApplication.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsApplication.class, args);
+    }
+
+}
